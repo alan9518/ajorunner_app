f92e6b7c734be601ccea435db776873436e99000
alan medina <alan.medina@derevo.com.mx>
2025-12-18 11:53:56 -0600

    chore: style mao view

--- app/(tabs)/Home/components/GMapView/GMapView.tsx
+++ app/(tabs)/Home/components/GMapView/GMapView.tsx
@@ -46,7 +46,5 @@ const styles = StyleSheet.create({
   map: {
     width: "100%",
     height: "100%",
-    borderColor: "red",
-    borderWidth: 2,
   },
 });
--- app/(tabs)/Home/index.tsx
+++ app/(tabs)/Home/index.tsx
@@ -3,7 +3,9 @@ import { GMapView } from "./components/GMapView/GMapView";
 const Home = () => {
   return (
     <View className="flex-1 h-full m-0 p-0 items-center justify-center dark:bg-background-dark bg-background">
-      <GMapView />
+      <View className="w-full h-3/4 absolute top-0">
+        <GMapView />
+      </View>
     </View>
   );
 };
