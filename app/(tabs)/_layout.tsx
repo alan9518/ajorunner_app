02ea7010846d0d9bab5ad9dd77e211042344f545
alan medina <alan.medina@derevo.com.mx>
2025-12-18 11:53:37 -0600

    chore: set styling to tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,28 @@
 import { Tabs } from "expo-router";
+import { ViewStyle } from "react-native";
+const tabBarItemStyle: ViewStyle = {
+  width: "100%",
+  height: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+};
 
 const _Layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: {
+          height: 90,
+          backgroundColor: "#18181B",
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+          overflow: "hidden",
+          position: "absolute",
+          borderTopWidth: 0,
+        },
+        tabBarItemStyle: tabBarItemStyle,
+      }}
+    >
       <Tabs.Screen
         name="Home/index"
         options={{
