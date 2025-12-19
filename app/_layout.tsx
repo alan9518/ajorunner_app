49e8c7bd52aa190fb037ec89665c26cf26e18989
alan medina <alan.medina@derevo.com.mx>
2025-12-19 16:19:00 -0600

    chore: add custom font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,35 @@
+import {
+  Lexend_400Regular,
+  Lexend_700Bold,
+  useFonts,
+} from "@expo-google-fonts/lexend";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded, errors] = useFonts({
+    Lexend_400Regular,
+    Lexend_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || errors) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, errors]);
+
+  if (!fontsLoaded && !errors) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       {/* Root background */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 const { colors } = require("./src/theme/colors");
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./app/**/*.{js, jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js, jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/shared/components/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      fontFamily: {
+        lexend: ["Lexend_400Regular"],
+        "lexend-bold": ["Lexend_700Bold"],
+      },
+      fontSize: {
+        xl: 20,
+        "2xl": 24,
+        "3xl": 30,
+      },
       colors: {
         background: {
           DEFAULT: colors.light.background,
@@ -19,9 +32,9 @@ module.exports = {
           DEFAULT: colors.light.background,
           dark: colors.dark.background,
         },
-        text: {
-          DEFAULT: colors.light.text,
-          dark: colors.dark.text,
+        foreground: {
+          DEFAULT: colors.light.white,
+          dark: colors.dark.foreground,
         },
       },
     },
